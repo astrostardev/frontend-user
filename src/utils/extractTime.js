ddb401b8b1a1e23345b2679b6d755d787b72d3bb
astrostardev <153346634+astrostardev@users.noreply.github.com>
2024-04-12 14:46:23 +0530

    Add files via upload

--- src/utils/extractTime.js
+++ src/utils/extractTime.js
@@ -8,12 +8,40 @@ export function extractTime(dateString) {
         return ''; // Return empty string if dateString is not a valid date
     }
 
-    let hours = date.getHours();
-    const minutes = padZero(date.getMinutes());
-    let amOrPm = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12 || 12; // Convert hours to 12-hour format
-    hours = padZero(hours);
-    return `${hours}:${minutes} ${amOrPm}`;
+    const currentDate = new Date(); // Current date
+
+    const year = date.getFullYear();
+    const month = padZero(date.getMonth() + 1); // Month is zero-based
+    const day = padZero(date.getDate());
+
+    // Check if the conversation happened today
+    if (date.toDateString() === currentDate.toDateString()) {
+        let hours = date.getHours();
+        const minutes = padZero(date.getMinutes());
+        let amOrPm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12 || 12; // Convert hours to 12-hour format
+        hours = padZero(hours);
+
+        return `${hours}:${minutes} ${amOrPm}`;
+    }
+
+    // Check if the conversation happened yesterday
+    const yesterday = new Date(currentDate);
+    yesterday.setDate(yesterday.getDate() - 1);
+    if (date.toDateString() === yesterday.toDateString()) {
+        return 'Yesterday';
+    }
+
+    // Check if the conversation happened within the past week
+    const sevenDaysAgo = new Date(currentDate);
+    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+    if (date > sevenDaysAgo) {
+        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        return days[date.getDay()];
+    }
+
+    // Otherwise, return the date in "YYYY-MM-DD" format
+    return `${year}-${month}-${day}`;
 }
 
 function padZero(number) {
